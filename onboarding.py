d99686a328aeb163a3708260b40805d32e0dda24
bvolovat <borisv@armosec.io>
2023-07-31 17:46:49 +0300

    test
    Signed-off-by: bvolovat <borisv@armosec.io>

--- onboarding.py
+++ onboarding.py
@@ -18,6 +18,7 @@ def setup_driver():
     driver = webdriver.Chrome(options=chrome_options)
     return driver
 
+
 def login(driver, wait, email_onboarding, login_pass_onboarding):
     url = "https://cloud.armosec.io/dashboard"
     driver.get(url)
@@ -28,20 +29,10 @@ def login(driver, wait, email_onboarding, login_pass_onboarding):
     password_input_box.send_keys(login_pass_onboarding)
     password_input_box.send_keys(Keys.ENTER)
 
+
 def click_get_started(driver, wait):
     get_started_button = wait.until(EC.element_to_be_clickable((By.XPATH, '/html/body/armo-root/div/div/div/armo-home-page/armo-home-empty-state/armo-empty-state-page/main/section[1]/div/button/span[1]')))
     driver.execute_script("arguments[0].click();", get_started_button)
-
-# def copy_helm_command(driver, wait):
-#     # Replace the following string with the correct CSS selector for the element containing the Helm command
-#     css_selector_for_helm_command = '.some-class #some-id'
-#     helm_command_element = wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, css_selector_for_helm_command)))
-
-#     # Use JavaScript to get the text content of the element
-#     helm_command = driver.execute_script("return arguments[0].textContent;", helm_command_element)
-
-#     # print(f"Copied helm command: {helm_command.strip()}")  # Print the command
-#     return helm_command.strip()
 
 
 def copy_helm_command(driver, wait):
@@ -49,6 +40,7 @@ def copy_helm_command(driver, wait):
     helm_command_element = wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, css_selector)))
     helm_command = helm_command_element.text  # Retrieve the text content of the Helm command
     return helm_command
+
 
 def execute_helm_command(helm_command):
     try:
@@ -60,12 +52,12 @@ def execute_helm_command(helm_command):
             print(e.stderr.decode('utf-8'))  # Print stderr to see what went wrong
 
 
-
 def verify_installation(driver, wait):
     verify_button = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'armo-dialog-footer .mat-button-wrapper')))
     # driver.save_screenshot("verify_button.png")
     driver.execute_script("arguments[0].click();", verify_button)
     
+
 def view_cluster(driver, wait):
     try:
         view_cluster_button = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'armo-connection-wizard-connection-step-footer .connection-step-button')))
@@ -84,6 +76,7 @@ def view_connected_cluster(driver, wait):
     except TimeoutException as e:
         print("View cluster connected button was not found or clickable.")
         # driver.save_screenshot("view_cluster_connected_error.png")
+
 
 def main():
     email_onboarding = os.environ.get('email_onboarding')
