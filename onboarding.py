eaf207fb0af50d20964509d7741377bc0959a2ae
bvolovat <borisv@armosec.io>
2023-07-31 17:07:24 +0300

    test
    Signed-off-by: bvolovat <borisv@armosec.io>

--- onboarding.py
+++ onboarding.py
@@ -1,13 +1,16 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import TimeoutException
-import time, datetime
 import os 
 import subprocess
+import time, datetime
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.support import expected_conditions as EC
+
+
+
 
 def setup_driver():
     chrome_options = Options()
