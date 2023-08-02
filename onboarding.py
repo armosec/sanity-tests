b437e6addf54ce109aa094a40d420cdb36cefa1b
bvolovat <borisv@armosec.io>
2023-08-02 17:43:09 +0300

    Added retry for cluster deletion
    Signed-off-by: bvolovat <borisv@armosec.io>

--- onboarding.py
+++ onboarding.py
@@ -1,4 +1,4 @@
-import os 
+import os
 import subprocess
 import time, datetime
 from selenium import webdriver
@@ -14,6 +14,8 @@ def setup_driver():
     chrome_options = Options()
     chrome_options.add_argument("--headless")
     driver = webdriver.Chrome(options=chrome_options)
+    driver.set_window_size(1512, 982)
+
     return driver
 
 
@@ -34,33 +36,37 @@ def click_get_started(driver, wait):
 
 
 def copy_helm_command(driver, wait):
-    css_selector = 'div.command-area > span.ng-star-inserted'  # CSS selector targeting the Helm command
+    css_selector = 'div.command-area > span.ng-star-inserted'
     helm_command_element = wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, css_selector)))
-    helm_command = helm_command_element.text  # Retrieve the text content of the Helm command
+    helm_command = helm_command_element.text
     return helm_command
 
 
 def execute_helm_command(helm_command):
     try:
         result = subprocess.run(helm_command, shell=True, check=True, stderr=subprocess.PIPE)
-        # print("Helm command executed successfully.")
     except subprocess.CalledProcessError as e:
         print(f"Helm command execution failed with error: {e}")
-        if e.stderr:  # Check if stderr is not None
-            print(e.stderr.decode('utf-8'))  # Print stderr to see what went wrong
+        if e.stderr:
+            print(e.stderr.decode('utf-8'))
 
 
 def verify_installation(driver, wait):
-    verify_button = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'armo-dialog-footer .mat-button-wrapper')))
-    # driver.save_screenshot("verify_button.png")
-    driver.execute_script("arguments[0].click();", verify_button)
-    
+    try:
+        verify_button = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'armo-dialog-footer .mat-button-wrapper')))
+        # driver.save_screenshot("verify_button.png")
+        driver.execute_script("arguments[0].click();", verify_button)
+    except TimeoutException as e:
+        print("Verify button was not found or clickable.")
+        # driver.save_screenshot("verify_button_error.png")
+
 
 def view_cluster(driver, wait):
     try:
-        view_cluster_button = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'armo-connection-wizard-connection-step-footer .armo-button')))
-        # driver.save_screenshot("view_cluster_button.png")
-        time.sleep(0.3)
+        view_cluster_button = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'armo-connection-wizard-connection-step-footer .armo-button'))) 
+        wait.until(EC.visibility_of_element_located((By.XPATH, '//*[@id="mat-dialog-0"]/armo-config-scanning-connection-wizard-dialog/armo-onboarding-dialog/main/main/armo-connection-wizard-dialog-connection-step/div/img')))
+        time.sleep(0.5)
+        driver.save_screenshot("view_cluster_button.png")
         driver.execute_script("arguments[0].click();", view_cluster_button)
     except TimeoutException as e:
         print("View cluster button was not found or clickable.")
@@ -68,12 +74,13 @@ def view_cluster(driver, wait):
 
 
 def view_connected_cluster(driver, wait):
+    wait = WebDriverWait(driver, 60, 0.001)
     try:
-        delete_cluster_button = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'armo-cluster-scans-table .mat-tooltip-trigger')))
+        wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'armo-cluster-scans-table .mat-tooltip-trigger')))
         # driver.save_screenshot("view_cluster_connected.png")
     except TimeoutException as e:
-        print("View cluster connected button was not found.")
-        # driver.save_screenshot("view_cluster_connected_error.png")
+        print("Failed to find view cluster connected after retries. Refreshing page.")
+        driver.refresh()
 
 
 def uninstall_kubescape():
@@ -89,27 +96,47 @@ def uninstall_kubescape():
 
 def click_settings_button(driver, wait):
     settings_button = wait.until(EC.element_to_be_clickable((By.XPATH, '/html/body/armo-root/div/armo-side-nav-menu/nav/div[2]/armo-nav-items-list/div/ul/li/a/span')))
-    # driver.save_screenshot("click_settings_button.png")
+    driver.save_screenshot("click_settings_button.png")
     driver.execute_script("arguments[0].click();", settings_button)
 
 
 def click_more_options_button(driver, wait):
     more_options_button = wait.until(EC.element_to_be_clickable((By.XPATH, '/html/body/armo-root/div/div/div/div[2]/armo-clusters-page/armo-clusters-table/div/table/tbody/tr/td[9]/armo-row-options-button/button/mat-icon')))
+    driver.save_screenshot("click_more_options_button.png")
     driver.execute_script("arguments[0].click();", more_options_button)
 
 
 def choose_delete_option(driver, wait):
     delete_button_option = wait.until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[5]/div[2]/div/div/div/button[2]/div')))
+    driver.save_screenshot("choose_delete_option.png")
     driver.execute_script("arguments[0].click();", delete_button_option)
 
 
 def confirm_delete(driver, wait):
     confirm_delete_button = wait.until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[5]/div[2]/div/mat-dialog-container/armo-notification/div[3]/button[2]')))
+    driver.save_screenshot("confirm_delete_button.png")
     driver.execute_script("arguments[0].click();", confirm_delete_button)
 
 
-def wait_for_empty_table(driver, wait):
+def wait_for_empty_table(driver):
+    wait = WebDriverWait(driver, 180, 0.001)
     wait.until(EC.text_to_be_present_in_element((By.CSS_SELECTOR, 'td.mat-cell.text-center.ng-star-inserted'), 'No data to display'))
+    driver.save_screenshot("wait_for_empty_table.png")
+
+
+def perform_cleanup(driver, wait):
+    max_retries = 2
+    for _ in range(max_retries):
+        try:
+            uninstall_kubescape()
+            click_settings_button(driver, wait)
+            click_more_options_button(driver, wait)
+            choose_delete_option(driver, wait)
+            confirm_delete(driver, wait)
+            wait_for_empty_table(driver)
+            break
+        except Exception as e:
+            print(f"Cleanup cluster attempt failed with error: {e}, retrying...")
 
 
 def main():
@@ -128,6 +155,8 @@ def main():
     view_cluster(driver, wait)
     view_connected_cluster(driver, wait)
     end_time = time.time()
+    perform_cleanup(driver, wait)
+    driver.quit()
     
     onboarding_time = "{:.2f}".format(end_time - start_time)
     lonboarding_time_without_login = "{:.2f}".format(end_time - login_time)
@@ -136,13 +165,6 @@ def main():
         f.write(f"{timestamp},{onboarding_time},{lonboarding_time_without_login}\n")
     print(f"{timestamp},{onboarding_time},{lonboarding_time_without_login}\n")
 
-    uninstall_kubescape()
-    click_settings_button(driver, wait)
-    click_more_options_button(driver, wait)
-    choose_delete_option(driver, wait)
-    confirm_delete(driver, wait)
-    wait_for_empty_table(driver, wait)
-    driver.quit()
 
 if __name__ == "__main__":
     main()
