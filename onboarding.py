6658a1f368c2569da3b5a8f5fa0765e6daac1857
bvolovat <borisv@armosec.io>
2023-09-03 17:03:09 +0300

    test
    Signed-off-by: bvolovat <borisv@armosec.io>

--- onboarding.py
+++ onboarding.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import subprocess
+import traceback
 import time, datetime
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -24,7 +25,6 @@ def get_current_timestamp():
 
 
 def login(driver, wait, email_onboarding, login_pass_onboarding, url):
-    print(f"Scanning BE URL: {url}"}
     # url = "https://cloud.armosec.io/dashboard"
     driver.get(url)
     email_input_box = wait.until(EC.visibility_of_element_located((By.XPATH, '//*[@id="frontegg-login-box-container-default"]/div[1]/input')))
@@ -33,6 +33,28 @@ def login(driver, wait, email_onboarding, login_pass_onboarding, url):
     password_input_box = wait.until(EC.visibility_of_element_located((By.XPATH, '/html/body/frontegg-app/div[2]/div[2]/input')))
     password_input_box.send_keys(login_pass_onboarding)
     password_input_box.send_keys(Keys.ENTER)
+    # check if onboarding-role page is displayed
+    element = driver.find_element_by_xpath("//div[@class='label font-semi-bold font-size-18 my-3' and contains(text(), 'What do you do?')]")
+    if element:
+        role_page(driver, wait) # call role_page function if the element exists
+    else:    
+        print("Onboarding role page is not displayed - not a sign up user")
+
+        
+def role_page(driver, wait):
+    role_button = wait.until(EC.element_to_be_clickable((By.XPATH, '/html/body/armo-root/div/div/div/armo-role-page/div/div[2]/div/div[1]/armo-onboarding-survey-buttons-upper/div/div[1]/div[1]')))
+    driver.execute_script("arguments[0].click();", role_button)
+    people_amount_button = wait.until(EC.element_to_be_clickable((By.XPATH, '/html/body/armo-root/div/div/div/armo-role-page/div/div[2]/div/div[2]/armo-onboarding-survey-buttons-lower/div/div[1]/div[1]')))
+    driver.execute_script("arguments[0].click();", people_amount_button)
+    continue_button = wait.until(EC.element_to_be_clickable((By.XPATH, '/html/body/armo-root/div/div/div/armo-role-page/div/div[2]/div/div[3]/button/span[2]'))) 
+    driver.execute_script("arguments[0].click();", continue_button)
+    experience_checkbox = driver.find_element_by_id('mat-checkbox-2-input')
+    experience_checkbox.click()
+    continue_button = wait.until(EC.element_to_be_clickable((By.XPATH, '/html/body/armo-root/div/div/div/armo-features-page/div/div[2]/div/div[3]/button/span[2]')))
+    driver.execute_script("arguments[0].click();", continue_button) 
+    #close the helm installation window
+    close_button = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="mat-dialog-0"]/armo-config-scanning-connection-wizard-dialog/armo-onboarding-dialog/armo-dialog-header/mat-icon')))
+    driver.execute_script("arguments[0].click();", close_button) 
 
 
 def click_get_started(driver, wait):
@@ -61,8 +83,8 @@ def verify_installation(driver, wait):
         verify_button = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'armo-dialog-footer .mat-button-wrapper')))
         driver.execute_script("arguments[0].click();", verify_button)
     except TimeoutException as e:
-        print("Verify button was not found or clickable.")
-        driver.save_screenshot(f"./logs/screenshots/verify_button_erro_{get_current_timestamp()}.png")
+        print("Verify-button was not found or clickable.")
+        driver.save_screenshot(f"./verify_button_erro_{get_current_timestamp()}.png")
 
 
 def view_cluster_button(driver, wait):
@@ -73,7 +95,7 @@ def view_cluster_button(driver, wait):
         driver.execute_script("arguments[0].click();", view_cluster_button)
     except TimeoutException as e:
         print("View cluster button was not found or clickable.")
-        driver.save_screenshot(f"./logs/screenshots/view_cluster_button_error_{get_current_timestamp()}.png")
+        driver.save_screenshot(f"./view_cluster_button_error_{get_current_timestamp()}.png")
         
 
 
@@ -83,7 +105,7 @@ def view_connected_cluster(driver, wait):
         wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'armo-cluster-scans-table .mat-tooltip-trigger')))
     except TimeoutException as e:
         print("Failed to find view cluster connected after retries. Refreshing page.")
-        driver.save_screenshot(f"./logs/screenshots/view_connected_cluster_error_{get_current_timestamp()}.png")
+        driver.save_screenshot(f"./view_connected_cluster_error_{get_current_timestamp()}.png")
         driver.refresh()
 
 
@@ -128,22 +150,37 @@ def perform_cleanup(driver, wait):
     max_retries = 2
     for _ in range(max_retries):
         try:
-            uninstall_kubescape()
+            print("Clicking settings button...")
             click_settings_button(driver, wait)
+
+            print("Clicking more options button...")
             click_more_options_button(driver, wait)
+
+            print("Choosing delete option...")
             choose_delete_option(driver, wait)
+
+            print("Confirming delete...")
             confirm_delete(driver, wait)
+
+            print("Waiting for empty table...")
             wait_for_empty_table(driver)
             break
         except Exception as e:
-            print(f"Cleanup cluster attempt failed with error: {e}, retrying...")
-            driver.save_screenshot(f"./logs/screenshots/cleanup_cluster_error_{get_current_timestamp()}.png")
+            print(f"Cleanup cluster attempt {max_retries} failed with error:")
+            print("Stack trace:")
+            print(traceback.format_exc())
+                                                
+            cleanup_error_screenshot = f"./cleanup_err_{get_current_timestamp()}.png"
+            print(f"Saving screenshot - cleanup_err_{get_current_timestamp()}.png")
+            driver.save_screenshot(cleanup_error_screenshot)
 
 
 
 def main():
-    email_onboarding = os.environ.get('email_onboarding')
-    login_pass_onboarding = os.environ.get('login_pass_onboarding')
+    # email_onboarding = os.environ.get('email_onboarding')
+    # login_pass_onboarding = os.environ.get('login_pass_onboarding')
+    email_onboarding = "borisv@armosec.io"
+    login_pass_onboarding = "Bv110584!"
     prod_url = "https://cloud.armosec.io/dashboard"
     url = sys.argv[1] if len(sys.argv) > 1 else prod_url
 
@@ -159,6 +196,7 @@ def main():
     view_cluster_button(driver, wait)
     view_connected_cluster(driver, wait)
     end_time = time.time()
+    uninstall_kubescape()
     perform_cleanup(driver, wait)
     driver.quit()
     
