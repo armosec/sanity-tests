eed00cff9db4c08d7400f053cf5cd0a74f5b53d0
bvolovat <borisv@armosec.io>
2023-10-05 11:25:13 +0300

    upadte onboarding flow
    Signed-off-by: bvolovat <borisv@armosec.io>

---
+++ cluster_operator.py
@@ -0,0 +1,201 @@
+# cluster_operator.py
+import time, datetime
+import subprocess
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.support import expected_conditions as EC
+
+_setup_driver = None
+
+def initialize_driver():
+    global setup_driver
+    chrome_options = Options()
+    # chrome_options.add_argument("--headless")
+    _setup_driver = webdriver.Chrome(options=chrome_options)
+    _setup_driver.set_window_size(1512, 982)
+    return _setup_driver
+    
+
+class ClusterManager:
+    def __init__(self,driver):
+        self.driver = driver
+        self.wait = WebDriverWait(self.driver, timeout=90, poll_frequency=0.001)
+
+    def login(self, email_latency, login_pass_latency, url):
+        driver = self.driver
+        wait = self.wait
+        driver.get(url)
+        wait.until(EC.visibility_of_element_located((By.XPATH, '//*[@id="frontegg-login-box-container-default"]/div[1]/input')))
+        mail_input = driver.find_element(by=By.XPATH, value='//*[@id="frontegg-login-box-container-default"]/div[1]/input')
+        mail_input.send_keys(email_latency)
+        mail_input.send_keys(Keys.ENTER)
+        wait.until(EC.visibility_of_element_located((By.XPATH, '/html/body/frontegg-app/div[2]/div[2]/input')))
+        password_input = driver.find_element(by=By.XPATH, value='/html/body/frontegg-app/div[2]/div[2]/input')
+        password_input.send_keys(login_pass_latency)
+        password_input.send_keys(Keys.ENTER)
+
+        # check if onboarding-role page is displayed
+        try:
+            wait_for_element = WebDriverWait(driver, 5, 0.001)
+            element = wait_for_element.until(EC.visibility_of_element_located((By.XPATH, "//div[@class='label font-semi-bold font-size-18 my-3' and contains(text(), 'What do you do?')]")))
+        except:
+            print("Onboarding role page is not displayed - not a sign-up user")
+        else:
+            print("Onboarding role page is displayed - sign-up user (first login)")
+            ClusterManager.handle_role_page(self)
+        
+        
+    def handle_role_page(self):
+        driver = self.driver
+        wait = self.wait
+        try:
+            role_button = wait.until(EC.element_to_be_clickable((By.XPATH, '/html/body/armo-root/div/div/div/armo-role-page/div/div[2]/div/div[1]/armo-onboarding-survey-buttons-upper/div/div[1]/div[2]')))
+            driver.execute_script("arguments[0].click();", role_button)
+            print("Click on role button.")
+            # driver.save_screenshot(f"./role_button_{ClusterManager.get_current_timestamp()}.png")
+        except TimeoutException as e:
+            print("Role button was not found or clickable.")
+            driver.save_screenshot(f"./role_button_error_{ClusterManager.get_current_timestamp()}.png")
+        try:
+            people_amount_button = wait.until(EC.element_to_be_clickable((By.XPATH, '/html/body/armo-root/div/div/div/armo-role-page/div/div[2]/div/div[2]/armo-onboarding-survey-buttons-lower/div/div[1]/div[1]')))
+            driver.execute_script("arguments[0].click();", people_amount_button)
+            print("Click on people amount button.")
+        except TimeoutException as e:
+            print("People amount button was not found or clickable.")
+            driver.save_screenshot(f"./people_amount_button_error_{ClusterManager.get_current_timestamp()}.png")
+        try:    
+            continue_button = wait.until(EC.element_to_be_clickable((By.XPATH, '/html/body/armo-root/div/div/div/armo-role-page/div/div[2]/div/div[3]/button/span[2]'))) 
+            driver.execute_script("arguments[0].click();", continue_button)
+            print("Click on continue button.")
+        except TimeoutException as e:
+            print("Continue button was not found or clickable.")
+            driver.save_screenshot(f"./continue_button_error_{ClusterManager.get_current_timestamp()}.png")
+        try:
+            experience_checkbox = wait.until(EC.element_to_be_clickable((By.XPATH, '/html/body/armo-root/div/div/div/armo-features-page/div/div[2]/div/div[2]/armo-onboarding-how-best-help-buttons/div[1]/div')))
+            driver.execute_script("arguments[0].click();", experience_checkbox)
+            print("Click on experience checkbox.")
+        except TimeoutException as e:
+            print("Experience checkbox was not found or clickable.")
+            driver.save_screenshot(f"./experience_checkbox_error_{ClusterManager.get_current_timestamp()}.png")
+        try:    
+            continue_button = wait.until(EC.element_to_be_clickable((By.XPATH, '/html/body/armo-root/div/div/div/armo-features-page/div/div[2]/div/div[3]/button/span[2]')))
+            driver.execute_script("arguments[0].click();", continue_button) 
+            print("Click on continue button.")
+        except TimeoutException as e:
+            print("Continue button was not found or clickable.")
+            driver.save_screenshot(f"./continue_button_error_{ClusterManager.get_current_timestamp()}.png")
+        try:    #close the helm installation window
+            close_button = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="mat-dialog-0"]/armo-config-scanning-connection-wizard-dialog/armo-onboarding-dialog/armo-dialog-header/header/mat-icon')))
+            driver.execute_script("arguments[0].click();", close_button)
+            print("Click on close button.")
+        except TimeoutException as e:
+            print("Onboarding role page was not found or clickable.")
+            driver.save_screenshot(f"./onboarding_role_page_error_{ClusterManager.get_current_timestamp()}.png")
+
+    @staticmethod
+    def get_current_timestamp():
+        return datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+
+class ConnectCluster:
+    def __init__(self,driver):
+        self.driver = driver
+        self.wait = WebDriverWait(self.driver, timeout=90, poll_frequency=0.001)
+
+    def click_get_started(self):
+        print("Click on get started button.")
+        try:
+            # get_started_button = self.wait.until(EC.element_to_be_clickable((By.XPATH, '/html/body/armo-root/div/div/div/armo-home-page/armo-home-empty-state/armo-empty-state-page/main/section[1]/div/button/span[1]')))
+            get_started_button = self.wait.until(EC.element_to_be_clickable((By.XPATH, '/html/body/armo-root/div/div/div/armo-home-page/armo-home-empty-state/armo-empty-state-page/main/section[1]/div/armo-button/button')))
+            self.driver.execute_script("arguments[0].click();", get_started_button)
+        except TimeoutException as e:
+            print("Get started button was not found or clickable.")
+            self.driver.save_screenshot(f"./get_started_button_error_{ClusterManager.get_current_timestamp()}.png")
+
+    def connect_cluster_helm(self):
+        css_selector = 'div.command-area > span.ng-star-inserted'
+        helm_command_element = self.wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, css_selector)))
+        helm_command = helm_command_element.text
+        try:
+            result = subprocess.run(helm_command, shell=True, check=True, stderr=subprocess.PIPE)
+        except subprocess.CalledProcessError as e:
+            print(f"Helm command execution failed with error: {e}")
+            if e.stderr:
+                print(e.stderr.decode('utf-8'))
+
+    def verify_installation(self):
+        try:
+            verify_button = self.wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'armo-dialog-footer .mat-button-wrapper')))
+            self.driver.execute_script("arguments[0].click();", verify_button)
+        except TimeoutException as e:
+            print("Verify button was not found or clickable.")
+            self.driver.save_screenshot(f"./verify_button_erro_{ClusterManager.get_current_timestamp()}.png")
+
+
+    def view_cluster_button(self):
+        try:
+            view_cluster_button = self.wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'armo-connection-wizard-connection-step-footer .armo-button'))) 
+            self.wait.until(EC.visibility_of_element_located((By.XPATH, '//*[@id="mat-dialog-0"]/armo-config-scanning-connection-wizard-dialog/armo-onboarding-dialog/main/main/armo-connection-wizard-dialog-connection-step/div/img')))
+            time.sleep(1)
+            self.driver.execute_script("arguments[0].click();", view_cluster_button)
+        except TimeoutException as e:
+            print("View cluster button was not found or clickable.")
+            self.driver.save_screenshot(f"./view_cluster_button_error_{ClusterManager.get_current_timestamp()}.png")
+
+
+
+    def view_connected_cluster(self):
+        try:
+            self.wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'armo-cluster-scans-table .mat-tooltip-trigger')))
+        except TimeoutException as e:
+            print("Failed to find view cluster connected after retries. Refreshing page.")
+            self.driver.save_screenshot(f"./view_connected_cluster_error_{ClusterManager.get_current_timestamp()}.png")
+            self.driver.refresh()
+
+class Cleanup:
+    def __init__(self, driver):
+        self.driver = driver
+        self.wait = WebDriverWait(self.driver, timeout=90, poll_frequency=0.001)
+
+    def uninstall_kubescape(self): 
+        print("Uninstalling kubescape...")
+        command = "helm uninstall kubescape -n kubescape && kubectl delete ns kubescape"
+        process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        stdout, stderr = process.communicate()
+
+        if process.returncode != 0:
+            print(f"Error executing command: {stderr.decode()}")
+        else:
+            print(f"Command executed successfully: {stdout.decode()}")
+
+    def click_settings_button(self):
+        settings_button = self.wait.until(EC.element_to_be_clickable((By.XPATH, '/html/body/armo-root/div/armo-side-nav-menu/nav/div[2]/armo-nav-items-list/div/ul/li/a/span')))
+        self.driver.execute_script("arguments[0].click();", settings_button)
+        self.driver.execute_script("arguments[0].click();", settings_button)
+        print("Click on settings button.")
+
+    def click_more_options_button(self):
+        more_options_button = self.wait.until(EC.element_to_be_clickable((By.XPATH, '/html/body/armo-root/div/div/div/div[2]/armo-clusters-page/armo-clusters-table/div/table/tbody/tr/td[9]/armo-row-options-button/button/mat-icon')))
+        self.driver.execute_script("arguments[0].click();", more_options_button)
+        print("Click on more options button.")
+
+    def choose_delete_option(self):
+        # delete_button_option = self.wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="mat-menu-panel-107"]/div/button[2]/div')))
+        # delete_button_option = self.wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="mat-menu-panel-73"]/div/button[2]/div')))
+        # self.driver.execute_script("arguments[0].click();", delete_button_option)
+        delete_button = self.driver.find_element(By.XPATH, "//button[text()='Delete']")
+        delete_button.click()
+        print("Click on delete button option.")
+
+    def confirm_delete(self):
+        confirm_delete_button = self.wait.until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[5]/div[2]/div/mat-dialog-container/armo-notification/div[3]/button[2]')))
+        self.driver.execute_script("arguments[0].click();", confirm_delete_button)
+        print("Click on confirm delete button.")
+
+    def wait_for_empty_table(self):
+        wait = WebDriverWait(self.driver, 180, 0.001)
+        wait.until(EC.text_to_be_present_in_element((By.CSS_SELECTOR, 'td.mat-cell.text-center.ng-star-inserted'), 'No data to display'))
+        print("Cleanup done")
---
+++ onboarding_comliance_valne.py
@@ -0,0 +1,161 @@
+# test-onboarding.py
+import os
+import sys
+import time
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from cluster_operator import ClusterManager, ConnectCluster, Cleanup, initialize_driver
+from selenium.webdriver.support.ui import WebDriverWait
+
+
+def navigate_to_dashboard(driver, wait):
+    # Click on the compliance
+    try:
+        wait.until(EC.presence_of_element_located((By.ID, 'configuration-scanning-left-menu-item')))
+        compliance = driver.find_element(By.ID, 'configuration-scanning-left-menu-item')
+        driver.execute_script("arguments[0].click();", compliance)
+    except:
+        print("failed to click on compliance")
+        driver.save_screenshot(f"./failed_to_click_on_compliance_{ClusterManager.get_current_timestamp()}.png")
+
+    # Click on the cluster (the first one)
+    wait.until(EC.presence_of_element_located((By.XPATH, '/html/body/armo-root/div/div/div/armo-config-scanning-page/div[2]/armo-cluster-scans-table/table/tbody/tr[1]/td[2]')))
+    cluster = driver.find_element(By.XPATH, '/html/body/armo-root/div/div/div/armo-config-scanning-page/div[2]/armo-cluster-scans-table/table/tbody/tr[1]/td[2]')
+    # driver.save_screenshot(f"./click_on_cluster_{ClusterManager.get_current_timestamp()}.png")
+    driver.execute_script("arguments[0].click();", cluster)
+    
+
+    # Click on the fix button
+    try:
+        wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="configuration-scanning-controls-table"]/table/tbody/tr[2]/td[9]/armo-fix-button/armo-button')))
+        fix_button = driver.find_element(By.XPATH, '//*[@id="configuration-scanning-controls-table"]/table/tbody/tr[2]/td[9]/armo-fix-button/armo-button')
+        driver.execute_script("arguments[0].click();", fix_button)
+    except:
+        print("failed to click on fix button")
+        driver.save_screenshot(f"./failed_to_click_on_fix_button_{ClusterManager.get_current_timestamp()}.png")
+
+    # Wait until the rules list is visible
+    wait.until(EC.presence_of_element_located((By.XPATH, '/html/body/armo-root/div/div/div/armo-resources-ignore-rules-page/div[3]/armo-resources-ignore-rules-list/div/armo-resources-ignore-rules-list-with-namespace/table/thead/tr')))
+    # take_screenshot(driver, "Wait until the rules list is visible")
+
+    # # Click on the fix button in the rules list
+    # wait.until(EC.presence_of_element_located((By.XPATH, '/html/body/armo-root/div/div/div/armo-vulnerabilities-page/div[2]/armo-vulnerabilities-table/table/tbody/tr[1]/td[1]')))
+    # # take_screenshot(driver, "Click on the fix button in the rules list")
+    # fix_button_on_remide = driver.find_element(By.XPATH, '/html/body/armo-root/div/div/div/armo-vulnerabilities-page/div[2]/armo-vulnerabilities-table/table/tbody/tr[1]/td[1]')
+    # driver.execute_script("arguments[0].click();", fix_button_on_remide)
+    # window_handles = driver.window_handles
+    # driver.switch_to.window(window_handles[-1])
+
+    # # Wait until the yaml section is visible
+    # time.sleep(1)
+    # wait.until(EC.presence_of_element_located((By.XPATH, '/html/body/armo-root/div/div/div/armo-failed-resource-page/armo-yaml-section/div/div/button[2]')))
+    # # take_screenshot(driver, "Wait until the yaml section is visible")
+    
+
+def navigate_to_vulnerabilities(driver, wait):
+    # driver.close()
+    # driver.switch_to.window(driver.window_handles[0])
+    # Click on the vulnerabilities page
+    wait.until(EC.presence_of_element_located((By.ID, 'image-scanning-left-menu-item')))
+    vulnerabilities = driver.find_element(By.ID, 'image-scanning-left-menu-item')
+    driver.execute_script("arguments[0].click();", vulnerabilities)
+
+    # Click on the first row
+    try:
+        wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="image-scanning-vulnerabilities-table-row-0"]/td[1]/span/span')))
+        first_row = driver.find_element(By.XPATH, '//*[@id="image-scanning-vulnerabilities-table-row-0"]/td[1]/span/span')
+        driver.execute_script("arguments[0].click();", first_row)
+    except:
+        print("failed to click on the first row")
+        driver.save_screenshot(f"./failed_to_click_on_the_first_row_on_vulne{ClusterManager.get_current_timestamp()}.png")
+        exit(1)
+
+    #click on ignore of the first CVE
+    first =wait.until(EC.element_to_be_clickable((By.XPATH, '/html/body/armo-root/div/div/div/armo-vce-report-page/div/armo-vce-table/table/tbody/tr[1]/td[9]/armo-ignore-rules-button/button')))                                    
+    driver.execute_script("arguments[0].click();", first)
+    time.sleep(0.5)
+
+    # close the ignore window
+    try:
+        wait.until(EC.presence_of_element_located((By.XPATH, '/html/body/div[5]/div[2]/div/mat-dialog-container/armo-cve-ignore-rule-dialog/div[1]/mat-icon')))
+        close = driver.find_element(By.XPATH, '/html/body/div[5]/div[2]/div/mat-dialog-container/armo-cve-ignore-rule-dialog/div[1]/mat-icon')
+        driver.execute_script("arguments[0].click();", close)
+    except:
+        print("failed to close the ignore window")
+        driver.save_screenshot(f"./failed_to_close_the_ignore_window_{ClusterManager.get_current_timestamp()}.png") 
+
+def perform_cleanup(driver):
+    cleanup = Cleanup(driver)
+    try:
+        cleanup.uninstall_kubescape()
+        cleanup.click_settings_button()   
+        cleanup.click_more_options_button()
+        cleanup.choose_delete_option()
+        cleanup.confirm_delete()
+        cleanup.wait_for_empty_table()
+
+    except:
+        print("Failed to perform cleanup")
+        driver.save_screenshot(f"./failed_to_perform_cleanup_{ClusterManager.get_current_timestamp()}.png")
+        exit(1) 
+        
+def main():
+    # Assigning your variables
+    email_onboarding = os.environ.get('email_onboarding')
+    login_pass_onboarding = os.environ.get('login_pass_onboarding')
+    prod_url = "https://cloud.armosec.io/dashboard"
+    url = sys.argv[1] if len(sys.argv) > 1 else prod_url
+
+    # Setup the driver
+    driver = initialize_driver()
+    wait = WebDriverWait(driver, 160, 0.001)
+
+    # Login
+    start_time = time.time()
+    cluster_manager = ClusterManager(driver)
+    cluster_manager.login(email_onboarding, login_pass_onboarding, url)
+    login_time = time.time() - start_time
+
+    # Connecting the cluster
+    connect_cluster = ConnectCluster(driver)
+    connect_cluster.click_get_started()
+    connect_cluster.connect_cluster_helm()
+    connect_cluster.verify_installation()
+    connect_cluster.view_cluster_button()
+    connect_cluster.view_connected_cluster()
+    onboarding_time = time.time() - start_time
+
+    # Navigating to dashboard
+    comp_start_time = time.time()
+    navigate_to_dashboard(driver, wait)
+    compalince_time = time.time() - comp_start_time
+
+    # Navigating to vulnerabilities 
+    vul_start_time = time.time()
+    navigate_to_vulnerabilities(driver, wait)
+    vulnerabilities_time = time.time() - vul_start_time
+
+    log_data = {
+    'timestamp': ClusterManager.get_current_timestamp(),
+    'login_time': login_time,
+    'onboarding_time': onboarding_time,
+    'onboarding_time_excluding_login': onboarding_time - login_time,
+    'compliance_time': compalince_time,
+    'vulnerabilities_time': vulnerabilities_time
+}
+    print(f"Timestamp: {log_data['timestamp']}\n"
+      f"Login Time: {log_data['login_time']:.2f}\n"
+      f"Onboarding Time: {log_data['onboarding_time']:.2f}\n"
+      f"Onboarding Time (Excluding Login): {log_data['onboarding_time_excluding_login']:.2f}\n"
+      f"Compliance Time: {log_data['compliance_time']:.2f}\n"
+      f"Vulnerabilities Time: {log_data['vulnerabilities_time']:.2f}\n") 
+    
+    with open("./logs/onboarding_logs.csv", "a") as f:
+        f.write(','.join(str(log_data[key]) for key in log_data) + '\n')  
+
+    # Cleanup
+    perform_cleanup(driver)
+    driver.quit()        
+
+if __name__ == "__main__":
+    main()
