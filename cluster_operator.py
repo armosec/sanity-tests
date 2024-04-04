e7127d014cad05e9545c16971aa8352cfee1e55d
bvolovat <borisv@armosec.io>
2024-04-04 14:44:53 +0300

    update user flow
    Signed-off-by: bvolovat <borisv@armosec.io>

--- cluster_operator.py
+++ cluster_operator.py
@@ -2,7 +2,6 @@
 import time, datetime
 import subprocess
 from selenium import webdriver
-
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
