a6752955f6209d392c5f5e41be150ae5055c5671
bvolovat <borisv@armosec.io>
2024-09-24 12:22:11 +0300

    update security risk add max size
    Signed-off-by: bvolovat <borisv@armosec.io>

--- tests/selenium_config.py
+++ tests/selenium_config.py
@@ -7,5 +7,6 @@ def initialize_driver():
     chrome_options.add_argument("--disable-gpu")
     chrome_options.add_argument("--no-sandbox")
     driver = webdriver.Chrome(options=chrome_options)
-    driver.set_window_size(1280, 800)
+    # driver.set_window_size(1280, 800)
+    driver.maximize_window()
     return driver
