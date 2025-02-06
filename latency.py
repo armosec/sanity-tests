1c6d140c632dd424a3a746842c9d477db0a4bdf2
bvolovat <borisv@armosec.io>
2025-02-06 15:12:18 +0200

    update the xpath
    Signed-off-by: bvolovat <borisv@armosec.io>

--- latency.py
+++ latency.py
@@ -51,12 +51,12 @@ class LatencyTest:
         mail_input = self._interaction_manager.wait_until_interactable(
             '//*[@id="frontegg-login-box-container-default"]/div[1]/input'
         )
-        mail_input.send_keys("test.platform454@gmail.com")
+        mail_input.send_keys(os.environ['email_latency'])
         mail_input.send_keys(Keys.ENTER)
         password_input = self._interaction_manager.wait_until_interactable(
             '/html/body/frontegg-app/div[2]/div[2]/input'
         )
-        password_input.send_keys("Platformtest1!")
+        password_input.send_keys(os.environ['login_pass_latency'])
         password_input.send_keys(Keys.ENTER)
         _logger.info("Logged in to Armo")
 
@@ -69,7 +69,8 @@ class LatencyTest:
         # Click on the failed resource button.
         self._interaction_manager.click('//*[@id="framework-control-table-failed-0"]/div/armo-router-link/a/armo-button/button', click_delay=1)
         # Click on the fix button in the rules list.
-        self._interaction_manager.click("//button[contains(@class, 'armo-button') and contains(@class, 'primary') and contains(@class, 'sm') and span[text()='Fix']]", click_delay=1)
+        # self._interaction_manager.click("//button[contains(@class, 'armo-button') and contains(@class, 'primary') and contains(@class, 'sm') and span[text()='Fix']]", click_delay=1)
+        self._interaction_manager.click('//*[@id="framework-control-table-failed-2"]/div/armo-router-link/a/armo-button', click_delay=1)
                                          
         # Switch to the last window.
         # self._interaction_manager.switch_to_window(-1)
@@ -87,7 +88,7 @@ class LatencyTest:
         latency_without_login = "{:.2f}".format(end_time - login_time)
         latency = "{:.2f}".format(end_time - start_time)
         latency_details = LatencyDetails(latency, latency_without_login)
-        # latency_details.to_file("./logs/latency_logs.csv")
+        latency_details.to_file("./logs/latency_logs.csv")
         _logger.info(f"Latency details: {latency_details}")
         self._interaction_manager.quit()
 
